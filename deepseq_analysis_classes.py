f4c1740698a6b3c15834f1d6a4dc515c6fe82342
Weronika Patena <patena@gmail.com>
2011-09-27 15:26:18 -0700

    Added reading data from processed file; wrote unit-tests; added options.
    Implemented reading data back from a file generated by
    deepseq_count_alignments.py, in addition to just reading from a SAM
    file, since I'll want to read that data and add more information (like
    gene locaions):
     - wrote add_counts and add_sequence_and_counts functions in
       Alignment_position_sequence_group, to just add counts/sequences
       directly instead of from HTSeq alignments
     - wrote a reed_from_file function in Alignment_position_sequence_group
       to read the full data from a file (and add it to existing data if
       any) - currently ignores specific sequences/counts and only takes the
       first 5 mandatory fields, may change that later.
     - added a position_type=None option (default) to
       All_alignments_grouped_by_pos, since if you're not reading from a raw
       SAM file the position_type is irrelevant; if position_type is None
       and you try to run add_alignment_reader_to_data, you get an error.
    
    Had to rethink the handling of sequence variants in
    Alignment_position_sequence_group, since they're tricky when reading
    from a file that doesn't always include the actual sequences:
     - added a unique_sequence_count variable to just keep track of the
       number of unique sequences, in addition to the
       sequences_and_counts dictionary (renamed from sequence_counts) which
       won't work of we don't have the actual sequences
     - modified add_read to increment unique_sequence_count when appropriate
       (depending on whether the sequence is already in sequences_and_counts)
     - put a add_to_uniqseqcount variable in add_sequence_and_counts that
       makes it either increment unique_sequence_count when appropriate or
       not touch it at all
     - put a assume_new_sequences variable in
       Alignment_position_sequence_group.add_counts and in
       All_alignments_grouped_by_pos.read_from_file, that governs whether
       new unique_sequence_counts should be assumed to be different from
       previously counted sequences or not (if it's True, if a group already
       has a unique_sequence_counts of 2 and it's given another 1, it
       assumes that 1 is one of the old 2 so it keeps it at 2, or if it has
       2 and gets 3 new it ends up with 3; if it's False, it assumes each
       sequence is different and goes with 2+1=3 and 2+3=5)
    
    Added unit-tests for most of the functions, except ones that require
    extensive mockups. Functions with added unit-tests:
     - in Alignment_position_sequence_group class:
        - __init__
        - add_counts
        - add_sequence_and_counts
        - get_main_sequence
     - in All_alignments_grouped_by_pos class:
        - __init__
        - read_from_file
    
    Functions WITHOUT unit-tests:
     - add_read in Alignment_position_sequence_group class
     - add_alignment_reader_to_data in All_alignments_grouped_by_pos class -
       requires HTSeq alignment mock-up
     - print_summary and print_data in All_alignments_grouped_by_pos class -
       they're only printing functions...
    Should add all of those, but it's not urgent, since I do have a full run
    test on deepseq_count_alignments.py that largely takes care of this.
    
    Also added some comments, fixed docstrings, etc.

--- deepseq_analysis_classes.py
+++ deepseq_analysis_classes.py
@@ -142,31 +142,63 @@ def get_chrom_and_pos_from_HTSeq_position(HTSeq_pos, pos_type):
 
 class Alignment_position_sequence_group():
     """ Data regarding sequences aligned to a particular genomic position (genomic position is set at initialization). 
-    Variables: chromosome, position, total_read_count, perfect_read_count, sequence_counts (a sequence:count dictionary).
+    Variables: chromosome, position, total_read_count, perfect_read_count, unique_sequence_count, 
+      sequences_and_counts (a sequence:count dictionary).
     Methods: add_read to add a given HTSeq read to the counts (doesn't check chromosome/position), 
-     get_main_sequence to get the most common sequence from sequence_counts. """
+     get_main_sequence to get the most common sequence from sequences_and_counts. """
 
     def __init__(self, chromosome, position):
-        """ Set chromosome and position; initialize total_read_count, perfect_read_count and sequence_counts to 0 or {}."""
+        """ Set chromosome and position; initialize total_read_count, perfect_read_count and sequences_and_counts."""
+        # should this check that chromosome is a string and position is an int?  
+        #   Probably not, in case I want to use HTSeq or Biopython position objects later...
         self.chromosome, self.position = chromosome, position
         self.total_read_count = 0
         self.perfect_read_count = 0
-        self.sequence_counts = defaultdict(lambda: 0)
+        self.unique_sequence_count = 0
+        self.sequences_and_counts = defaultdict(lambda: 0)
 
     def add_read(self, HTSeq_alignment, treat_unknown_as_match=False):
         """ Add a read to the data: increment total_read_count, increment perfect_read_count if read is a perfect 
-        alignment, increment the appropriate field of sequence_counts based on read sequence."""
-        self.sequence_counts[HTSeq_alignment.read.seq] += 1
+        alignment, increment the appropriate field of sequences_and_counts based on read sequence."""
+        # if it's a new sequence, increment unique_sequence_count; add a count to the self.sequences_and_counts dictionary.
+        seq = HTSeq_alignment.read.seq
+        if seq not in self.sequences_and_counts:
+            self.unique_sequence_count += 1
+        self.sequences_and_counts[seq] += 1
+        # increment self.total_read_count; figure out if the read is perfect and increment self.perfect_read_count if yes.
         self.total_read_count += 1
-        # figure out whether the read is perfect, treating unknowns ('M' in CIGAR string) as desired
         treat_unknown_as = 'match' if treat_unknown_as_match else 'mutation'
         mutation_count = check_mutation_count_try_all_methods(HTSeq_alignment, treat_unknown_as=treat_unknown_as)
         if mutation_count==0:  
             self.perfect_read_count += 1
 
+    def add_counts(self, total_count, perfect_count, sequence_variant_count, assume_new_sequences=False):
+        """ Increment self.total_read_count, self.perfect_read_count and self.unique_sequence_count based on inputs.
+        Note that ifself.unique_sequence_count>0, it's impossible to determine the correct new value: 
+         if we had old data with one unique sequence and now we have new data with another one, how do we know
+          if that's the same or different sequence?  The correct total could be 1 or 2, so it's an option:
+         If assume_new_sequences is True, the total is old+new; if it's False, the total is max(old,new).
+        """
+        self.total_read_count += total_count
+        self.perfect_read_count += perfect_count
+        if assume_new_sequences:
+            self.unique_sequence_count += sequence_variant_count
+        else:
+            self.unique_sequence_count = max(self.unique_sequence_count,sequence_variant_count)
+
+    def add_sequence_and_counts(self, seq, seq_count, add_to_uniqseqcount=True):
+        """ Add seq_count to self.sequences_and_counts[seq] (it's created with count 0 if seq wasn't a key before).
+        Note: if add_to_uniqseqcount is False, this will never increment self.unique_sequence_count;
+         otherwise it only does so if seq was not already present in the self.sequences_and_counts data
+          and if the total number of sequences in self.sequences_and_counts is higher than self.unique_sequence_count. """
+        if add_to_uniqseqcount:
+            if seq not in self.sequences_and_counts and len(self.sequences_and_counts)>self.unique_sequence_count:
+                self.unique_sequence_count += 1
+        self.sequences_and_counts[seq] += seq_count
+
     def get_main_sequence(self, N=1):
         """ Return the most common sequence in this group and its count (or Nth most common sequence if N is provided)."""
-        sequences_by_count = sorted([(count,seq) for (seq,count) in self.sequence_counts.iteritems()], reverse=True)
+        sequences_by_count = sorted([(count,seq) for (seq,count) in self.sequences_and_counts.iteritems()], reverse=True)
         # try returning the Nth sequence and count; return nothing if there are under N sequences.
         try:                return (sequences_by_count[N-1][1], sequences_by_count[N-1][0])
         except IndexError:  return ('',0)
@@ -175,13 +207,13 @@ class Alignment_position_sequence_group():
 class All_alignments_grouped_by_pos():
     """ Essentially a dictionary of alignment_position_sequence_group with position data (chrom,pos) as keys. """
 
-    def __init__(self, position_type):
+    def __init__(self, position_type=None):
         """ Checks position_type and assigns to self.position_type; initializes self.alignment_position_data_dict.
         position_type must be one of %s.
         self.alignment_position_data_dict is a dictionary that generates a new alignment_position_sequence_group object
          based on the key (i.e. position) if the key isn't already in the dictionary.  """%VALID_POSITION_TYPES
         self.alignment_position_data_dict = keybased_defaultdict(lambda key: Alignment_position_sequence_group(*key))
-        if not position_type in VALID_POSITION_TYPES: 
+        if not position_type in VALID_POSITION_TYPES+[None]: 
             raise ValueError("The position_type variable must be one of %s!"%VALID_POSITION_TYPES)
         self.position_type = position_type
         self.total_read_count, self.aligned_read_count, self.unaligned_read_count = 0,0,0
@@ -189,6 +221,8 @@ class All_alignments_grouped_by_pos():
     def add_alignment_reader_to_data(self, HTSeq_alignment_reader, treat_unknown_as_match=False):
         """ Adds all alignments to self.alignment_position_data_dict based on position.
         Input must be a list/generator/etc of HTSeq.Alignment objects (usually an HTSeq.SAM_Reader)."""
+        if self.position_type is None:
+            raise Exception("Cannot add data from an alignment reader if position_type isn't specified! Please set the position_type attribute of this All_alignments_grouped_by_pos instance to one of %s first."%VALID_POSITION_TYPES)
         for aln in HTSeq_alignment_reader:
             self.total_read_count += 1
             if (not aln.aligned) or (aln.iv is None):
@@ -232,12 +266,37 @@ class All_alignments_grouped_by_pos():
 
         for group in data:
             group_data = [group.chromosome, group.position, group.total_read_count, 
-                          group.perfect_read_count, len(group.sequence_counts)]
+                          group.perfect_read_count, len(group.sequences_and_counts)]
             OUTPUT.write('\t'.join([str(x) for x in group_data]))
             for N in range(1,N_sequences+1):
                 OUTPUT.write('\t%s\t%s'%group.get_main_sequence(N))
                 # MAYBE-TODO also give the length and number of mutations for each sequence? Optionally?  Length is easy, but do I even keep track of mutation number?  I probably should...
             OUTPUT.write('\n')
+
+    def read_from_file(self, infile, assume_new_sequences=False):
+        """ Read data from a file generated by self.print_data, and add to self.alignment_position_data_dict.  
+        Note that if you read data from a file and add it to data that already exists, it's impossible to determine the 
+         correct number of unique sequence variants per group: if the data originally listed 1 unique sequence, and new
+          data read from a file adds another 2 sequences, is that a total of 2 or 3 unique sequences? 
+         If assume_new_sequences is True, the total is old+new; if it's False, the total is max(old,new).
+        Currently doesn't read the specific sequences and counts - even if it did, some information could 
+        always be missing, since the file only has N first sequences. """
+        for line in open(infile):
+            # MAYBE-TODO get unaligned read count from summary if present - do we actually want that?
+            # ignore comment and header lines
+            if line.startswith('#'):    continue
+            if line.startswith('chromosome\tposition\ttotal_reads'):    continue
+            fields = line.split('\t')
+            chromosome = fields[0]
+            pos,total_reads,perfect_reads,sequence_variants = [int(x) for x in fields[1:5]]
+            self.alignment_position_data_dict[(chromosome,pos)].add_counts(total_reads, perfect_reads, 
+                                                                           sequence_variants, assume_new_sequences)
+            # MAYBE-TODO split the remaining fields into twos (seq,count) (we don't know how many there will be) 
+            #   and use self.alignment_position_data_dict[(chromosome, position)].add_sequence_and_counts(seq,count) 
+            #   to add specific sequences and counts to the data.  Add to unit-test!
+        self.aligned_read_count = sum([x.total_read_count for x in self.alignment_position_data_dict.values()])
+        self.total_read_count = self.aligned_read_count + self.unaligned_read_count
+    
 
 # MAYBE-TODO at some point I'll probably want to generalize both of those classes to not necessarily be grouped by position...
 
@@ -382,16 +441,176 @@ class Testing_single_functions(unittest.TestCase):
 
 class Testing_Alignment_position_sequence_group(unittest.TestCase):
     """ Unit-tests for the Alignment_position_sequence_group class and its methods. """
-    def test_(self):
+
+    def test__init(self):
+        for chromosome in ['chr1', 'chromosome_2', 'chrom3', 'a', 'adfads', '100', 'scaffold_88']:
+            for position in [0,1,2,5,100,10000,4323423]:
+                group = Alignment_position_sequence_group(chromosome,position)
+                assert group.chromosome == chromosome
+                assert group.position == position
+                assert group.total_read_count == 0
+                assert group.perfect_read_count == 0
+                assert group.unique_sequence_count == 0
+                assert group.sequences_and_counts == {}
+
+    def test__add_read(self):
         pass
-    # TODO implement!
+        # MAYBE-TODO implement using a mock-up of HTSeq_alignment?  (see Testing_single_functions for how I did that)
+
+    def test__add_counts(self):
+        group = Alignment_position_sequence_group('chr',3)
+        group.add_counts(0,0,0)
+        assert group.total_read_count == 0
+        assert group.perfect_read_count == 0
+        assert group.sequences_and_counts == {}
+        group.add_counts(2,2,1)
+        assert group.total_read_count == 2
+        assert group.perfect_read_count == 2
+        assert group.sequences_and_counts == {}
+        group.add_counts(2,1,1)
+        assert group.total_read_count == 4
+        assert group.perfect_read_count == 3
+        assert group.sequences_and_counts == {}
+        # how group.unique_sequence_count changes depends on assume_new_sequences:
+        #  - if False, group.unique_sequence_count is max(new_seq_count, group.unique_sequence_count)
+        group = Alignment_position_sequence_group('chr',3)
+        group.add_counts(0,0,0,assume_new_sequences=False)
+        assert group.unique_sequence_count == 0
+        group.add_counts(1,1,1,assume_new_sequences=False)
+        assert group.unique_sequence_count == 1
+        group.add_counts(2,2,2,assume_new_sequences=False)
+        assert group.unique_sequence_count == 2
+        group.add_counts(1,1,1,assume_new_sequences=False)
+        assert group.unique_sequence_count == 2
+        group.add_counts(2,2,2,assume_new_sequences=False)
+        assert group.unique_sequence_count == 2
+        #  - if True and group.unique_sequence_count is new_seq_count + group.unique_sequence_count
+        group = Alignment_position_sequence_group('chr',3)
+        group.add_counts(0,0,0,assume_new_sequences=True)
+        assert group.unique_sequence_count == 0
+        group.add_counts(1,1,1,assume_new_sequences=True)
+        assert group.unique_sequence_count == 1
+        group.add_counts(2,2,2,assume_new_sequences=True)
+        assert group.unique_sequence_count == 3
+        group.add_counts(1,1,1,assume_new_sequences=True)
+        assert group.unique_sequence_count == 4
+        group.add_counts(2,2,2,assume_new_sequences=True)
+        assert group.unique_sequence_count == 6
+        group.add_counts(2,2,2,assume_new_sequences=False)
+        assert group.unique_sequence_count == 6
+
+    def test__add_sequence_and_counts(self):
+        group = Alignment_position_sequence_group('chr',3)
+        # adding sequence/count to group.sequences_and_counts, WITHOUT touching group.unique_sequence_count
+        group.add_sequence_and_counts('AAA',2,add_to_uniqseqcount=False)
+        assert group.sequences_and_counts == {'AAA':2}
+        assert group.unique_sequence_count == 0
+        # adding sequence/count to group.sequences_and_counts, and incrementing group.unique_sequence_count if warranted:
+        #  - if adding a sequence that was already there, don't increment
+        group.add_sequence_and_counts('AAA',2,add_to_uniqseqcount=True)
+        assert group.sequences_and_counts == {'AAA':4}
+        assert group.unique_sequence_count == 0
+        #  - if adding a new sequence, increment
+        group.add_sequence_and_counts('GGG',2,add_to_uniqseqcount=True)
+        assert group.sequences_and_counts == {'AAA':4, 'GGG':2}
+        assert group.unique_sequence_count == 1
+        #  - if adding a new sequence but group.unique_sequence_count is already higher than expected, don't increment
+        group.unique_sequence_count = 5
+        group.add_sequence_and_counts('CCC',2,add_to_uniqseqcount=True)
+        assert group.sequences_and_counts == {'AAA':4, 'GGG':2, 'CCC':2}
+        assert group.unique_sequence_count == 5
+        # make sure it raises an error if given a non-numeric argument
+        for not_a_number in [None,'','a','GGG']:
+            self.assertRaises(TypeError,group.add_sequence_and_counts,'CCC',not_a_number)
+
+    def test__get_main_sequence(self):
+        group = Alignment_position_sequence_group('chr',3)
+        assert group.get_main_sequence() == ('',0)
+        assert group.get_main_sequence(1) == ('',0)
+        assert group.get_main_sequence(4) == ('',0)
+        group.add_sequence_and_counts('AAA',1)
+        group.add_sequence_and_counts('GGG',2)
+        assert group.sequences_and_counts == {'AAA':1, 'GGG':2}
+        assert group.get_main_sequence() == ('GGG',2)
+        assert group.get_main_sequence(1) == ('GGG',2)
+        assert group.get_main_sequence(2) == ('AAA',1)
+        assert group.get_main_sequence(3) == ('',0)
+        assert group.get_main_sequence(4) == ('',0)
+        group.add_sequence_and_counts('CCC',1)
+        group.add_sequence_and_counts('AAA',2)
+        assert group.sequences_and_counts == {'AAA':3, 'GGG':2, 'CCC':1}
+        assert group.get_main_sequence() == ('AAA',3)
+        assert group.get_main_sequence(1) == ('AAA',3)
+        assert group.get_main_sequence(2) == ('GGG',2)
+        assert group.get_main_sequence(3) == ('CCC',1)
+        assert group.get_main_sequence(4) == ('',0)
+        assert group.get_main_sequence(5) == ('',0)
 
 
 class Testing_All_alignments_grouped_by_pos(unittest.TestCase):
     """ Unit-tests for the All_alignments_grouped_by_pos class and its methods. """
-    def test_(self):
+
+    def test__init(self):
+        for pos_type in VALID_POSITION_TYPES+[None]:
+            data = All_alignments_grouped_by_pos(pos_type)
+            assert data.position_type == pos_type
+            assert data.alignment_position_data_dict == {}
+            assert data.total_read_count == 0
+            assert data.aligned_read_count == 0
+            assert data.unaligned_read_count == 0
+        for pos_type in [True, False, 0, 0.11, 23, 'asdfas', '', 'something', [2,1]]:
+            self.assertRaises(ValueError, All_alignments_grouped_by_pos, pos_type)
+
+    def test__add_alignment_reader_to_data(self):
         pass
-    # TODO implement!
+        # TODO make sure it fails if self.position_type isn't defined...
+        # MAYBE-TODO implement using a mock-up of HTSeq_alignment?  (see Testing_single_functions for how I did that)
+
+    def test__print_summary(self):
+        pass
+        # MAYBE-TODO implement based on stuff in test_data, like do_test_run in deepseq_count_alignments.py?
+
+    def test__print_data(self):
+        pass
+        # MAYBE-TODO implement based on stuff in test_data, like do_test_run in deepseq_count_alignments.py?
+
+    def test__read_from_file(self):
+        input_file = 'test_data/test_output__U_leftmost.txt'
+        data = All_alignments_grouped_by_pos(None)
+        data.read_from_file(input_file)
+        assert data.aligned_read_count == 29
+        assert data.unaligned_read_count == 0
+        assert data.total_read_count == 29
+        # just spot-checking some of the outputs
+        group = data.alignment_position_data_dict[('reads_2_seqs_1',199)]
+        assert group.chromosome == 'reads_2_seqs_1'
+        assert group.position == 199
+        assert group.total_read_count == 2
+        assert group.perfect_read_count == 2 
+        assert group.unique_sequence_count == 1
+        group = data.alignment_position_data_dict[('mutation_yes',199)]
+        assert group.chromosome == 'mutation_yes'
+        assert group.position == 199
+        assert group.total_read_count == 6
+        assert group.perfect_read_count == 0
+        assert group.unique_sequence_count == 1
+        group = data.alignment_position_data_dict[('strandedness_+_reference',99)]
+        assert group.chromosome == 'strandedness_+_reference'
+        assert group.position == 99
+        assert group.total_read_count == 1
+        assert group.perfect_read_count == 1
+        assert group.unique_sequence_count == 1
+        # try adding more data to a file that already has some...
+        data.read_from_file(input_file, assume_new_sequences=False)
+        group = data.alignment_position_data_dict[('reads_2_seqs_1',199)]
+        assert group.chromosome == 'reads_2_seqs_1'
+        assert group.position == 199
+        assert group.total_read_count == 4
+        assert group.perfect_read_count == 4
+        # how group.unique_sequence_count should act in this case depends on the value of assume_new_sequences
+        assert group.unique_sequence_count == 1
+        data.read_from_file(input_file, assume_new_sequences=True)
+        assert group.unique_sequence_count == 2
 
 
 if __name__ == "__main__":
