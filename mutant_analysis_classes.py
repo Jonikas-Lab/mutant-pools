bb4c53c06b395d70cc86152223dbd1842b1f9b3c
Weronika Patena <patena@gmail.com>
2012-06-22 16:44:16 -0700

    Moved nicer_gene_feature_counts method to Dataset_summary_data.
    Originally it was a Insertional_mutant_pool_dataset staticmethod, but
    that didn't really make sense, since mutant gene-feature info is part of
    summary.  Modified usage in print_summary.

--- mutant_analysis_classes.py
+++ mutant_analysis_classes.py
@@ -780,6 +780,31 @@ class Dataset_summary_data():
     def aligned_incl_removed(self):
         return self.aligned_read_count + sum(self.ignored_region_read_counts.values())
 
+    def nicer_gene_feature_counts(self, merge_boundary_features=True, merge_confusing_features=False):
+        """ Return (gene_feature,count) list, biologically sorted, optionally with all "boundary" features counted as one.
+
+        The source gene feature counts are based on the self.mutant_counts_by_feature dict.
+        If merge_confusing_features==True, any locations containing '??' will be listed as '??'.
+        If merge_boundary_features==True, any locations containing '/' and no '??' will be listed as 'boundary'.
+        The custom sort order (based on what seems sensible biologically) is: CDS, intron, UTR, other, boundary.
+        """
+        new_feature_count_dict = defaultdict(lambda: 0)
+        for feature, count in self.mutant_counts_by_feature.items():
+            # note that anything containing '??' AND '/' never gets merged as boundary
+            if '??' in feature:
+                if merge_confusing_features:                  new_feature_count_dict['??'] += count
+                else:                                         new_feature_count_dict[feature] += count
+            elif '/' in feature and merge_boundary_features:  new_feature_count_dict['boundary'] += count
+            else:                                             new_feature_count_dict[feature] += count
+        # proper feature order is first by "importance" (CDS, intron, UTR, other (default), boundary), 
+        #  then alphabetically within each importance category.
+        proper_feature_order = defaultdict(lambda: 3, 
+                                           {'CDS':0, 'intron':1, 'five_prime_UTR':2, 'three_prime_UTR':2, 'boundary':4})
+        feature_count_sorted_list = sorted(new_feature_count_dict.items(), key=lambda (f,n): (proper_feature_order[f],f))
+        return feature_count_sorted_list
+    # TODO unit-test
+
+
 
 class Insertional_mutant_pool_dataset():
     """ A dataset of insertional mutants - contains some total counts and a set of Insertional_mutant objects.
@@ -1258,32 +1283,6 @@ class Insertional_mutant_pool_dataset():
             return "Found %s most common mutants, each with %s"%(len(most_common_mutants), readcount_info)
 
 
-    @staticmethod
-    def nicer_gene_feature_counts(summary, merge_boundary_features=True, merge_confusing_features=False):
-        """ Return (gene_feature,count) list, biologically sorted, optionally with all "boundary" features counted as one.
-
-        The source gene feature counts are based on the summary.mutant_counts_by_feature dict.
-        If merge_confusing_features==True, any locations containing '??' will be listed as '??'.
-        If merge_boundary_features==True, any locations containing '/' and no '??' will be listed as 'boundary'.
-        The custom sort order (based on what seems sensible biologically) is: CDS, intron, UTR, other, boundary.
-        """
-        new_feature_count_dict = defaultdict(lambda: 0)
-        for feature, count in summary.mutant_counts_by_feature.items():
-            # note that anything containing '??' AND '/' never gets merged as boundary
-            if '??' in feature:
-                if merge_confusing_features:                  new_feature_count_dict['??'] += count
-                else:                                         new_feature_count_dict[feature] += count
-            elif '/' in feature and merge_boundary_features:  new_feature_count_dict['boundary'] += count
-            else:                                             new_feature_count_dict[feature] += count
-        # proper feature order is first by "importance" (CDS, intron, UTR, other (default), boundary), 
-        #  then alphabetically within each importance category.
-        proper_feature_order = defaultdict(lambda: 3, 
-                                           {'CDS':0, 'intron':1, 'five_prime_UTR':2, 'three_prime_UTR':2, 'boundary':4})
-        feature_count_sorted_list = sorted(new_feature_count_dict.items(), key=lambda (f,n): (proper_feature_order[f],f))
-        return feature_count_sorted_list
-    # TODO unit-test
-
-
     def print_summary(self, OUTPUT=sys.stdout, N_genes_to_print=5, line_prefix='    ', header_prefix=' * ', 
                       merge_boundary_features=True):
         """ Print basic read and mutant counts (prints to stdout by default, can also pass an open file object)."""
@@ -1347,7 +1346,7 @@ class Insertional_mutant_pool_dataset():
                              +"%s (%.2g)\n"%(count, count/summ.mutants_in_genes))
             # custom order for features to make it easier to read: CDS, intron, UTRs, everything else alphabetically after
             # MAYBE-TODO also give print_summary an option for merge_confusing_features arg to nicer_gene_feature_counts?
-            for (feature,count) in self.nicer_gene_feature_counts(summ, merge_boundary_features):
+            for (feature,count) in summ.nicer_gene_feature_counts(merge_boundary_features):
                 OUTPUT.write(line_prefix+"Mutant cassettes in gene feature %s (fraction of ones in genes): "%feature
                              +"%s (%.2g)\n"%(count, count/summ.mutants_in_genes))
             all_genes = set([mutant.gene for mutant in self]) - set(SPECIAL_GENE_CODES.all_codes)
