c0a00115c959c783fd7ef6515e02abcb76105322
Weronika Patena <patena@gmail.com>
2013-07-09 16:12:47 -0700

    Made compatible with Phytozome v5 genome format; cleanup.
    Changed several functionality aspects to work with both Phytozome v4.3
    and v5 chlamy genome files (different formats!).  General cleanup to
    make the code make more sense.  THE FUNCTION SIGNATURE CHANGED - need to
    change all the programs that use it!

--- parse_annotation_file.py
+++ parse_annotation_file.py
@@ -2,28 +2,46 @@
 
 import os
 
-def parse_gene_annotation_file(gene_annotation_filename, standard_Cre_file=False, header_fields=None, 
-                               genes_start_with='Cre', strip_last_gene_fields=0, gene_field_delimiter='.', 
+def parse_gene_annotation_file(gene_annotation_filename, standard_Phytozome_file=False, header_fields=None, gene_ID_column=0, 
+                               genes_start_with=None, remove_empty_columns=True, strip_gene_fields_start=None,
                                pad_with_empty_fields=True, ignore_comments=False, verbosity_level=1):
     """ Parse tab-separated gene annotation file; return a geneID:data_list dict, and a column header list (if present).
+
     Try to automatically detect if the file has a header; optionally use header provided; 
      return None for header if none was provided or detected. 
-    Remove columns with no contents.
-    """
-    # TODO update/rewrite docstring!
+    
+    If standard_Phytozome_file is non-zero, use special case headers for standard Phytozome files:
+        v4 annotation file (Creinhardtii_169_annotation_info.txt) if value is 4, v5 (Creinhardtii_236_readme.txt) if 5.
+        (those files don't have included headers! And the formats sometimes change, so this isn't a guarantee.)
 
+    Optionally shorten the gene name by truncating it starting with the strip_gene_fields_start value if found (if not None):
+        for example if strip_gene_fields_start is '.t', Cre01.g123450.t2.1 would become Cre01.g123450.
+
+    Optionally remove empty columns (i.e. fields that are empty in all lines).
+    """
     if not os.path.lexists(gene_annotation_filename):
         raise Exception("Couldn't find the %s gene annotation file!"%gene_annotation_filename)
     if verbosity_level>0:
         print " *** Parsing file %s for gene annotation info..."%gene_annotation_filename
 
-    # special case for the Creinhardtii_169_annotation_info.txt file and similar (from README for that file)
-    if standard_Cre_file:
-        header_fields = ['Phytozome transcript name', 'PFAM', 'Panther', 'KOG', 'KEGG ec', 'KEGG Orthology', 
-                         'best arabidopsis TAIR10 hit name', 'best arabidopsis TAIR10 hit symbol', 
-                         'best arabidopsis TAIR10 hit defline', 
-                         'best rice hit name', 'best rice hit symbol', 'best rice hit defline']
-        strip_last_gene_fields = 2
+    # special cases for the standard Phytozome annotation files
+    if standard_Phytozome_file:
+        strip_gene_fields_start = '.t'
+        if standard_Phytozome_file==4:
+            gene_ID_column = 0
+            header_fields = ['Phytozome transcript name', 'PFAM', 'Panther', 'KOG', 'KEGG ec', 'KEGG Orthology', 
+                             'best arabidopsis TAIR10 hit name', 'best arabidopsis TAIR10 hit symbol', 
+                             'best arabidopsis TAIR10 hit defline', 
+                             'best rice hit name', 'best rice hit symbol', 'best rice hit defline']
+        elif standard_Phytozome_file==5:
+            gene_ID_column = 1
+            header_fields = ['Phytozome transcript name', 
+                             'PFAM', 'Panther', 'KOG', 'KEGG ec', 'KEGG Orthology', 'Gene Ontology terms', 
+                             'best arabidopsis TAIR10 hit name', 'best arabidopsis TAIR10 hit symbol', 
+                             'best arabidopsis TAIR10 hit defline', 
+                             'best rice hit name', 'best rice hit symbol', 'best rice hit defline']
+        else:
+            raise Exception("Invalid value for standard_Phytozome_file type arg! %s given, 4/5 accepted."%standard_Phytozome_file)
 
     # parse the whole file into lists of tab-separated fields
     # MAYBE-TODO this reads in the whole file at once - not very efficient! Change to a generator?  
@@ -34,25 +52,18 @@ def parse_gene_annotation_file(gene_annotation_filename, standard_Cre_file=False
         fields = line.strip().split('\t')
         data_by_row.append(fields)
         
-    # if the first line doesn't start with an expected gene ID, assume it's a header
-    if not data_by_row[0][0].startswith(genes_start_with):
-        header = data_by_row[0]
+    # if header not given, assume the first line is a header
+    if header_fields is None:
+        header_fields = data_by_row[0]
         del data_by_row[0]
         if verbosity_level>0:
-            print "Assuming the first line is a header: %s"%'\t'.join(header)
-    else: 
-        header = None
+            print "Assuming the first line is a header: %s"%'\t'.join(header_fields)
     # if any of the other lines doesn't start with a Cre* gene ID, fail!
-    for row in data_by_row:
-        if not row[0].startswith(genes_start_with):
-            raise Exception("Can't parse file %s - found non-header line that doesn't start "%gene_annotation_filename
-                            +"with a %s gene ID!\n  \"%s\""%(genes_start_with, '\t'.join(row)))
-        # TODO do I want to somehow prevent silently replacing file header with header from arguments?...
-    # use the header from arguments if provided
-    if header_fields is not None:
-        header = header_fields
-        if verbosity_level>1:
-            print "Using header provided: %s"%'\t'.join(header)
+    if genes_start_with is not None:
+        for row in data_by_row:
+            if not row[0].startswith(genes_start_with):
+                raise Exception("Can't parse file %s - found line that doesn't start "%gene_annotation_filename
+                                +"with a %s gene ID!\n  \"%s\""%(genes_start_with, '\t'.join(row)))
 
     # check that all the data lengths line up (if they don't, don't throw an error
     mismatched_lengths = False
@@ -61,12 +72,11 @@ def parse_gene_annotation_file(gene_annotation_filename, standard_Cre_file=False
         if verbosity_level>1 or (not pad_with_empty_fields and verbosity_level>0):
             print "Warning: not all data rows have the same length! Lengths found: %s"%list(data_lengths)
         mismatched_lengths = True
-    if header is not None and len(header) not in data_lengths:
+    if len(header_fields) not in data_lengths:
         if verbosity_level>1 or (not pad_with_empty_fields and verbosity_level>0):
             print("Warning: header has a different number of fields than the data! "
                   +"Header length: %s. Data lengths: %s"%(len(header),list(data_lengths)))
         mismatched_lengths = True
-    
     if len(data_lengths)>1 and pad_with_empty_fields:
         max_length = max(max(data_lengths), len(header))
         if verbosity_level>0:
@@ -79,7 +89,7 @@ def parse_gene_annotation_file(gene_annotation_filename, standard_Cre_file=False
         mismatched_lengths = False
 
     # remove empty columns (only if all the data lengths match!)
-    if not mismatched_lengths:
+    if remove_empty_columns and not mismatched_lengths:
         data_length = len(header)
         columns_to_remove = []
         for pos in range(data_length):
@@ -102,24 +112,23 @@ def parse_gene_annotation_file(gene_annotation_filename, standard_Cre_file=False
     # convert the list-format data into a by-gene dictionary
     data_by_gene = {}
     for row in data_by_row:
-        gene = row[0]
-        if strip_last_gene_fields:
-            gene = gene_field_delimiter.join(gene.split(gene_field_delimiter)[:-strip_last_gene_fields])
-        data = row[1:]
+        gene = row[gene_ID_column]
+        data = row[0:gene_ID_column] + row[gene_ID_column+1:]
+        if strip_gene_fields_start is not None:
+            gene = gene.split(strip_gene_fields_start)[0]
         if gene in data_by_gene:
             if verbosity_level>0:
-                print "Warning: gene %s appears twice in the data! Using last appearance."%gene
+                print "Warning: gene %s appears twice in the data! Using the later appearance."%gene
         data_by_gene[gene] = data
 
     # remove the first word from the header, since it should be "gene ID" or such; 
     #  change spaces to underscores in header fields for readability
     if header:  
-        del header[0]
+        del header[gene_ID_column]
         header = [s.replace(' ','_') for s in header]
 
     if verbosity_level>0:
         print " *** DONE Parsing gene annotation file"
     return data_by_gene, header
 
-# TODO add unit-tests or something?  Right now I don't really know if this works at all!
-
+# TODO add unit-tests or run/tests?  It's not exactly complicated...
